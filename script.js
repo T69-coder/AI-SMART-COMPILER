32a9fbecaf76c8ca19f6d3bfad31b68a4ffeaf5b
T69-coder <tikeshgaikwad602@gmail.com>
2026-08-04 11:35:34 +0530

    Connect frontend to Render backend

--- script.js
+++ script.js
@@ -148,32 +148,22 @@ runBtn.addEventListener("click", async () => {
 
 
         const response = await fetch(
-            "http://127.0.0.1:8000/compile",
+            "https://ai-smart-compiler.onrender.com/compile",
             {
+                method: "POST",
 
-                method:"POST",
-
-                headers:{
-
-                    "Content-Type":"application/json"
-
+                headers: {
+                    "Content-Type": "application/json"
                 },
 
-
-                body:JSON.stringify({
-
-                    code:editor.getValue(),
-
-                    language:language.value
-
+                body: JSON.stringify({
+                    code: editor.getValue(),
+                    language: language.value
                 }),
 
-
-                signal:controller.signal
-
+                signal: controller.signal
             }
         );
-
 
 
         clearTimeout(timeout);
